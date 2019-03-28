5b1d11b52632bf060144ef1135fd0a1adc2b9fdb
Bluerain <41102800+gasd238@users.noreply.github.com>
2019-03-29 08:51:02 +0900

    출력 부분 변경

--- Modules/Annseq.py
+++ Modules/Annseq.py
@@ -4,8 +4,11 @@ def rand():
     sequence = ''
     man = ['재홍', '정현', '호승', '상현', '서빈', '영빈', '종효', '선우', '현주']
     num = ['1','2','3','4','5','6','7','8','9']
-    for j in range(9):
-        sequence = sequence + man[j] +' '+ num.pop(int(randint(0, len(num)-1))) + '\n'
+    for j in range(0, 9):
+        if j % 2 == 1:
+            sequence = sequence + man[j] +' '+ num.pop(int(randint(0, len(num)-1))) + '\n'
+        else:
+            sequence = sequence + man[j] +' '+ num.pop(int(randint(0, len(num)-1))) + ' '
     return sequence
 
 
