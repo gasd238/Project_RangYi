e8960a371d2b90eea0674ac006bb51816bc5f81d
Bluerain <41102800+gasd238@users.noreply.github.com>
2019-04-01 20:14:15 +0900

    Add files via upload

--- Modules/search.py
+++ Modules/search.py
@@ -7,22 +7,28 @@ def get_video_link(titleli):
     title = ''
     for i in titleli:
         title = title + i + ' '
-    print(title)
     chromedriver_dir = 'C:\chromedriver.exe'
     driver = webdriver.Chrome(chromedriver_dir)
     driver.get('https://www.youtube.com/results?search_query='+ title)
     source = driver.page_source
     bs = bs4.BeautifulSoup(source, 'lxml')
     entire = bs.find_all('a', {'id': 'video-title'})
+    driver.quit()
     embed = discord.Embed(title="영상 목록 이니라~~", description="검색한 영상 결과이니라~~", colour=0xf7cac9)
     if entire == []:
         embed.add_field(name='영상 없음..', value='검색된 영상이 없느니라...')
     else:
-        for i in range(0, 5):
-            entireNum = entire[i]
-            entireText = entireNum.text.strip()
-            test1 = entireNum.get('href')
-            link = 'https://www.youtube.com'+test1
-            embed.add_field(name=str(i+1)+'번째 영상',value=entireText + '\n링크 : '+link)
-    driver.quit()
+        if len(entire)<5:
+            for i in range(0, len(entire)):
+                entireText = entire[i].text.strip()
+                test1 = entire[i].get('href')
+                link = 'https://www.youtube.com'+test1
+                embed.add_field(name=str(i+1)+'번째 영상',value=entireText + '\n링크 : '+link)
+        else:
+             for i in range(0, 5):
+                entireText = entire[i].text.strip()
+                test1 = entire[i].get('href')
+                link = 'https://www.youtube.com'+test1
+                embed.add_field(name=str(i+1)+'번째 영상',value=entireText + '\n링크 : '+link)           
+    
     return embed
