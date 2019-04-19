b975c1a095b2b1802cb6cf39933c7306a98d8fa5
Bluerain <41102800+gasd238@users.noreply.github.com>
2019-04-19 09:14:35 +0900

    앙 급식띠

--- Modules/hungry.py
+++ Modules/hungry.py
@@ -2,6 +2,9 @@ import requests
 import datetime
 import re
 from bs4 import BeautifulSoup
+import discord
+
+weekend_string = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
 
 def hungry():
     soup=BeautifulSoup(requests.get('http://www.gsm.hs.kr/xboard/board.php?tbnum=8').text, 'html.parser')
@@ -16,10 +19,13 @@ def hungry():
             today = temp[now.day - 1].find_all('div', class_="content_info")
         if now.hour>=19 or now.hour<8:
             meal=today[0].getText()
+            tm = '아침'
         elif now.hour>=8 and now.hour<13:
             meal=today[1].getText()
+            tm ='점심'
         elif now.hour>=13 and now.hour<19:
             meal=today[2].getText()
+            tm = '저녁'
     meal=meal.split('\n')
     for i in range(0, len(meal)):
         if re.compile('[0-9]+').match(meal[i]):
@@ -28,8 +34,15 @@ def hungry():
             del meal[i:]
             break
     cmeal=[]
+    descriptions = ''
     for i in range(0,len(meal)):
         meal[i]=meal[i].split('\xa0')
         meal[i][0] = meal[i][0].strip('/')
         cmeal.append(meal[i][0])
-    return cmeal
+    if len(cmeal)==1:
+            descriptions=cmeal[0]
+    else:
+        for i in range(0, len(cmeal)):
+            descriptions=descriptions+'- '+cmeal[i]+'\n'
+    embed = discord.Embed(title="%s년 %s월 %s일 %s의 %s 식단표이니라~~" % (now.year, now.month, now.day, weekend_string[int(now.weekday())], tm),description=descriptions, colour=0xf7cac9)
+    return embed
