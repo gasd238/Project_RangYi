de4238d325004a35343f160b587b9bf6a17494d7
Bluerain <41102800+gasd238@users.noreply.github.com>
2019-04-22 11:48:50 +0900

    일정 관련

---
+++ Modules/calendar.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+def get_calendar():
+        today = datetime.datetime.now()
+
+        req = requests.get("http://www.gsm.hs.kr/xboard/board.php?tbnum=4")
+        soup = BeautifulSoup(req.text, 'lxml')
+
+        try:
+            info = soup.select("#xb_fm_list > div.calendar > ul > li > dl")
+            
+            result = "```"
+            for i in info:
+                if not i.find("dd") == None:
+                    text = i.text.replace("\n", "")
+                    result += "%6s -%s\n" % (text.split("-")[0], text.split("-")[1])
+                    for i in text.split("-")[2:]:
+                        result += "%7s -%s\n" % ("", i)
+            result += "```"
+            return result
+
+        except AttributeError:
+            return "%s년 %s월 학사일정을 불러올 수 없느니라..." % (today.year, today.month)
