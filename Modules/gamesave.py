1605ff4aa16ea1ec6e117e71d49251166efbd33b
gasd238 <gasd238@naver.com>
2019-05-23 18:57:40 +0900

    Hoxy??

--- Modules/gamesave.py
+++ Modules/gamesave.py
@@ -1,12 +1,12 @@
 import json
 class Save:
-    def save(self, ulev, uid, uname):
+    def save(self, ulev, uid, favper):
         #json 파일을 불러와 저장되있는 단계(level), 호감도(Favorability)를 변경
         with open('Data/game_save.json', 'r', encoding='utf-8') as game_save:
             data = game_save.read()
         data = json.loads(data)
         try:
-            data["played_users"][str(uid)]["name"] = str(uname)
+            data["played_users"][str(uid)]["favper"] = int(favper)
             data["played_users"][str(uid)]["level"] = int(ulev)
             with open('Data/game_save.json', 'w', encoding='utf-8') as game_save:
                 json.dump(data, game_save, ensure_ascii=False, indent="\t")
@@ -21,15 +21,15 @@ class Save:
             data = game_save.read()
         data = json.loads(data)
         try:
-            name = data["played_users"][str(uid)]["name"]
+            favper = data["played_users"][str(uid)]["favper"]
             lev = data["played_users"][str(uid)]["level"]
-            return name, lev
+            return favper, lev
         except:
             data["played_users"][str(uid)]={
-                "name" : 'Null',
-                "level" : 1
+                "favper" : 50,
+                "level" : 0
             }
             with open('Data/game_save.json', 'w', encoding='utf-8') as game_save:
                 json.dump(data, game_save, ensure_ascii=False, indent="\t")
-            return 'Null', 1
+            return 50, 0
             
