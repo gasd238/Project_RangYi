f218e870e7c3cd1c5ac7782604fb80b76c67db4f
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-18 18:06:54 +0900

    레벨 증가 DB화

--- Modules/user.py
+++ Modules/user.py
@@ -13,22 +13,21 @@ class UserLevel:
     def levelIncrease(self, user, message):
         userid = user.id
         isLevelup = False
-        with open('Data/userdata.json', 'r', encoding='utf-8') as userdata:
-            data = userdata.read()
-        data = json.loads(data)
-        if str(userid) not in data['users']:
-            data['users'][str(userid)] = {
+        result = collection.find_one({"userid": user.id})
+        if not result:
+            result = {
+                "userid": user.id,
                 "level": 1,
-                "currentxp": 0,
-                "targetxp": LevelExpGetter(2)
+                "currentxp": 0
             }
-        data['users'][str(userid)]['currentxp'] += len(message) // 4 + 1
-        while data['users'][str(userid)]['currentxp'] > data['users'][str(userid)]['targetxp']:
-            data['users'][str(userid)]['level'] += 1
-            data['users'][str(userid)]['targetxp'] += LevelExpGetter(data['users'][str(userid)]['level'])
+            collection.insert_one(result)
+        result['currentxp'] += len(message) // 4 + 1
+        targetExp = self.LevelExpGetter(result['level'])
+
+        while result['currentxp'] > targetExp:
+            result['level'] += 1
             isLevelup = True
-        with open('Data/userdata.json', 'w', encoding='utf-8') as userdata:
-            json.dump(data, userdata, ensure_ascii=False, indent="\t")
+        collection.update_one({"userid": userid}, {"$set": result})
         return isLevelup
 
     def showLevel(self, user, isLevelUp=False):
