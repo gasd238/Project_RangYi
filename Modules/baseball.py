4b996b387edb1c1ae43f4c638c7eb321be199492
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-07-28 22:16:56 +0900

    야구 기능 업데이트

--- Modules/baseball.py
+++ Modules/baseball.py
@@ -12,15 +12,34 @@ class Baseball:
             pass
         for i in home:
             if teamName in i.text:
-                team = i.find_all('span', class_="name")
-                pitcher = i.select('div > div > span:nth-child(4)')
-                score = i.find_all('div', class_="score")
-                inning = i.find('em', class_="live")
-                picture = i.find_all('img', class_="emblem")
-
-                embed1 = discord.Embed(title=team[0].text+"\n"+pitcher[0].text, description=score[0].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
-                embed1.set_thumbnail(url = str(picture[0]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
-                embed2 = discord.Embed(title=team[1].text+"\n"+pitcher[1].text, description=score[1].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
-                embed2.set_thumbnail(url = str(picture[1]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
-                return inning.text, embed1, embed2
+                inning = i.find('em')
+                if inning.text == "종료":
+                    team = i.find_all('span', class_="name")
+                    pitcher = i.select('div > div > span:nth-child(4)')
+                    picture = i.find_all('img', class_="emblem")
+                    score = i.select('div.inner > div')
+                    embed1 = discord.Embed(title=team[0].text+"\n"+pitcher[0].text, description=score[0].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
+                    embed1.set_thumbnail(url = str(picture[0]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    embed2 = discord.Embed(title=team[1].text+"\n"+pitcher[1].text, description=score[1].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
+                    embed2.set_thumbnail(url = str(picture[1]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    return inning.text, embed1, embed2
+                elif inning.text == "경기취소":
+                    team = i.find_all('span', class_="name")
+                    pitcher = i.select('div > div > span:nth-child(3)')
+                    picture = i.find_all('img', class_="emblem")
+                    embed1 = discord.Embed(title=team[0].text+"\n"+pitcher[0].text, description=inning.text, colour=0xf7cac9)
+                    embed1.set_thumbnail(url = str(picture[0]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    embed2 = discord.Embed(title=team[1].text+"\n"+pitcher[1].text, description=inning.text, colour=0xf7cac9)
+                    embed2.set_thumbnail(url = str(picture[1]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    return inning.text, embed1, embed2
+                else:
+                    team = i.find_all('span', class_="name")
+                    pitcher = i.select('div > div > span:nth-child(4)')
+                    score = i.find_all('div', class_="score")
+                    picture = i.find_all('img', class_="emblem")
+                    embed1 = discord.Embed(title=team[0].text+"\n"+pitcher[0].text, description=score[0].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
+                    embed1.set_thumbnail(url = str(picture[0]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    embed2 = discord.Embed(title=team[1].text+"\n"+pitcher[1].text, description=score[1].text.replace("\t", "").replace("\n", ""), colour=0xf7cac9)
+                    embed2.set_thumbnail(url = str(picture[1]['src'].replace("https://dthumb-phinf.pstatic.net?src=" ,"").replace("&type=f28_28&refresh=1", "")))
+                    return inning.text, embed1, embed2
         return None
