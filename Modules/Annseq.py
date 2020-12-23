dc58f3f5cd50bf512a8ea4289d7bc6e61c82228e
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-12-23 11:07:02 +0900

    나의 병신같은 코딩 과거 청산
    왜 나는 같은 걸 2번 진행했을까...

--- Modules/Annseq.py
+++ Modules/Annseq.py
@@ -2,23 +2,20 @@ from random import randint
 
 
 class Annseq:
-    def rand(self):
+    def rand(self, mlist):
         sequence = ''
-        man = ['재홍', '호승', '상현', '서빈', '영빈', '종효', '선우']
+        man = [] # 고정적으로 순서를 뽑는 목록이 있다면 넣어주세요.
+        if mlist != []:
+            man = mlist
+        for _ in range(random.randint(0, 21)):
+            random.shuffle(man)
         for j in range(len(man)):
+            sequence = sequence + str(j+1) +' '+ str(man[j])
             if j % 2 == 1:
-                sequence = sequence + str(j+1) +' '+ man.pop(int(randint(0, len(man)-1))) + '\n'
+                sequence += '\n'
             else:
-                sequence = sequence + str(j+1) +' '+ man.pop(int(randint(0, len(man)-1))) + ' '
-        return sequence
+                sequence += ' '
 
-    def rand_self(self, mlist):
-        sequence = ''
-        for j in range(len(mlist)):
-            if j % 2 == 1:
-                sequence = sequence + str(j+1) +' '+ mlist.pop(int(randint(0, len(mlist)-1))) + '\n'
-            else:
-                sequence = sequence + str(j+1) +' '+ mlist.pop(int(randint(0, len(mlist)-1))) + ' '
         return sequence
 
 
